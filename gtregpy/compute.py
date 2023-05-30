6b48ff60a94625ad22525d9af5b203bb9cdf7612
diegolara95 <diegolaradeandres@gmail.com>
2023-05-30 16:58:22 +0100

    Add LogLikelihood function.

--- gtregpy/compute.py
+++ gtregpy/compute.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+
 import cvxpy as cp
 import scipy.sparse as sp
 
@@ -15,5 +17,20 @@ def print_name(name):
         None
     """
     print(f"Hello, {name}!")
+def calc_loglike(b: np.ndarray, TYX: np.ndarray, tYX: np.ndarray) -> float:
+    """
+    Calculates the log-likelihood function.
 
+    Args:
+        b (np.ndarray): Coefficient matrix.
+        TYX (np.ndarray): TYX matrix.
+        tYX (np.ndarray): tYX matrix.
 
+    Returns:
+        float: The resulting sum of the log-likelihood function.
+    """
+    Kgauss = math.log(1 / math.sqrt(2 * math.pi))
+    e = np.matmul(TYX, b)
+    dedy = np.matmul(tYX, b)
+    llfvec = -.5 * e ** 2 + np.log(dedy) + Kgauss
+    return np.sum(llfvec)
