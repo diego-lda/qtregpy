ac91705590650312ea0d778a7740ef3be3601716
diegolara95 <diegolaradeandres@gmail.com>
2023-06-12 23:00:57 +0100

    feature: Add function to do Solving for gtreg
    This commit adds functions to cvxpy_socket so that they can be called from reticulate and run the solver in python.

--- gtregpy/cvxpy_socket.py
+++ gtregpy/cvxpy_socket.py
@@ -1,6 +1,42 @@
 import cvxpy as cp
 import numpy as np
+import csv
 
+def load_mel_data(file_path: str) -> tuple:
+    """
+    Load Melbourne data from a CSV file into two numpy arrays.
+
+    The CSV file should have two columns, and the function will skip the header row.
+    The values in the first column will be loaded into the first array and the values
+    in the second column will be loaded into the second array. Both columns should
+    contain numerical data.
+
+    Args:
+        file_path (str): Path to the CSV file.
+
+    Returns:
+        tuple: A tuple of two numpy arrays. The first array contains the data from
+        the first column of the CSV file, and the second array contains the data
+        from the second column of the CSV file.
+    """
+    # Create empty lists for x and y
+    x = []
+    y = []
+
+    # Open the CSV file and read it into the lists
+    with open(file_path, 'r') as f:
+        reader = csv.reader(f)
+        next(reader)  # Skip the header
+        for row in reader:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+
+    # Convert lists to numpy arrays
+    x = np.array(x)
+    y = np.array(y)
+
+    # Return the arrays
+    return x, y
 def score(e: np.array, h: np.array, TYX: np.array, tYX: np.array) -> np.array:
     """
     Compute the product of two matrices.
@@ -66,6 +102,13 @@ def solve_cvxpy(TYX: np.ndarray, tYX: np.ndarray, algorithm: str = None) -> tupl
 
     return result, e.value, h.value
 
+
+
+
+# Specify the path to your CSV file
+file_path = "/Users/diegolara/PycharmProjects/using_gtreg/data/melbourne_data.csv"
+
+x, y = load_mel_data(file_path)
 
 
 TYX = np.array(...)  # replace with your array
