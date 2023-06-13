73f0f3312ae0dca495ed742b00bb9a86f7d2df81
diegolara95 <diegolaradeandres@gmail.com>
2023-06-13 11:44:56 +0100

    feature: Given x and y get solution
    This commit adds functions to cvxpy_socket so that given two vectors of x and y data, it can return all of the information

--- gtregpy/cvxpy_socket.py
+++ gtregpy/cvxpy_socket.py
@@ -1,6 +1,8 @@
 import cvxpy as cp
 import numpy as np
 import csv
+from scipy.interpolate import BSpline
+from scipy.stats import norm
 
 def load_mel_data(file_path: str) -> tuple:
     """
@@ -37,6 +39,7 @@ def load_mel_data(file_path: str) -> tuple:
 
     # Return the arrays
     return x, y
+
 def score(e: np.array, h: np.array, TYX: np.array, tYX: np.array) -> np.array:
     """
     Compute the product of two matrices.
@@ -50,7 +53,7 @@ def score(e: np.array, h: np.array, TYX: np.array, tYX: np.array) -> np.array:
     Returns:
         np.array: The product of two matrices.
     """
-    grad = np.dot(TYX.T, e) + np.dot(tYX.T, h)
+    grad = TYX.T @ e + tYX.T @ h
     return grad
 
 def get_dllf(e: cp.Variable, h: cp.Variable, Kgauss: float = np.log(1/np.sqrt(2*np.pi))) -> cp.Expression:
@@ -100,17 +103,120 @@ def solve_cvxpy(TYX: np.ndarray, tYX: np.ndarray, algorithm: str = None) -> tupl
     else:
         result = problem.solve()
 
-    return result, e.value, h.value
+    # analyse the results
+    shadow1 = cond1.dual_value
+    bhat = -shadow1
+
+    return result, e.value, h.value, bhat
+
+def form_tz(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """
+    Generate the tz matrix.
+
+    If x or y are not 2D arrays (i.e., matrices), they are reshaped to be 2D.
+    The tz matrix is formed by multiplying each column of x with each column of y.
+
+    Args:
+        x (np.ndarray): First input array.
+        y (np.ndarray): Second input array.
+
+    Returns:
+        np.ndarray: The tz matrix.
+    """
+    # Ensure X and Y are 2D arrays
+    if len(y.shape) == 1:
+        y = y.reshape((1, -1))
+    if len(x.shape) == 1:
+        x = x.reshape((y.shape[0], -1))
+
+    nx = x.shape[1]
+    ny = y.shape[1]
+    nobs = x.shape[0]
+
+    tz = np.zeros((nobs, nx * ny))
+    i = 0
+
+    for j in range(ny):
+        for k in range(nx):
+            tz[:, i] = x[:, k] * y[:, j]
+            i += 1
+
+    return tz
+
+def get_iyknots(y: np.ndarray, y_order: int) -> np.ndarray:
+    """
+    Calculate quantiles for the given array.
+
+    Args:
+        y (np.ndarray): Input array for which to calculate quantiles.
+        y_order (int): The number of quantiles to calculate.
+
+    Returns:
+        np.ndarray: Array of quantile values.
+    """
+    return np.quantile(y, np.linspace(0, 1, y_order))
+
+def create_spline_basis(knots: np.ndarray, order: int):
+    """
+    Create B-spline basis functions for given knots and order.
+
+    Args:
+        knots (np.ndarray): Input array of knot values.
+        order (int): Order of the spline, or spline degree + 1.
+
+    Returns:
+        list: List of B-spline basis functions.
+    """
+    nknots = len(knots)
+    bsplines = [BSpline.basis_element(knots[i:i+order+1]) for i in range(nknots - order)]
+    return bsplines
 
 
 
 
-# Specify the path to your CSV file
+
+
+
+# Load the data from file
 file_path = "/Users/diegolara/PycharmProjects/using_gtreg/data/melbourne_data.csv"
-
 x, y = load_mel_data(file_path)
 
+# Build the xs matrix
+xs = np.concatenate((np.ones((x.shape[0], 1)), x.reshape(-1, 1)), axis=1)
 
-TYX = np.array(...)  # replace with your array
-tYX = np.array(...)  # replace with your array
-result, e_value, h_value = solve_cvxpy(TYX, tYX, algorithm='ECOS')
+# Build Ys and ys matrices
+nobs = len(y)
+cap_ys = np.c_[np.ones(len(y)), y]
+low_ys = np.array([0, 1] * nobs).reshape(nobs, 2)
+
+# Build the TYX and tYX matrices
+big_tyx = form_tz(x=xs, y=cap_ys)
+low_tyx = form_tz(x=xs, y=low_ys)
+
+# Solve the problem
+result, e_value, h_value, b_hat = solve_cvxpy(big_tyx, low_tyx, algorithm='ECOS')
+
+
+# Manipulate results to return
+finalscore = score(e_value, h_value, big_tyx, low_tyx)
+objval = get_dllf(e_value, h_value)
+llf_vec = np.log(norm.pdf(e_value) * (-1/h_value))
+llf = np.sum(llf_vec)
+e_hat = -np.dot(big_tyx, b_hat)
+eta_hat = np.dot(low_tyx, b_hat)
+
+ans = {
+    "llf": llf,
+    "e": e_value,
+    "eta": -1/h_value,
+    "finalscore": finalscore,
+    "result": result,
+    "e_hat": e_hat,
+    "eta_hat": eta_hat,
+    "h": h_value,
+    "objval": objval,
+    "llf_vec": llf_vec,
+    "b_mat": b_hat
+}
+
+print(ans)
