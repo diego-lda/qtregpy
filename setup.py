516a7564ed1370100abc4f7d8b4b7718bb99f9be
Diego Lara <diegolaradeandres@gmail.com>
2023-10-27 16:30:20 +0100

    chore: Bump Version Now that its in PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="qtregpy",
-    version="0.0.4",
+    version="0.1.0",
     author="Sami Stouli, Richard Spady, Xiaoran Liang, Diego Lara",
     author_email="s.stouli@bristol.ac.uk, rspady@jhu.edu, x.liang2@exeter.ac.uk, diegolaradeandres@gmail.com",
     description="A Python package to implement the Quantile Transformation Regression.",
